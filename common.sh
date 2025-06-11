4148fb5a998e9f20ae59d3511e25a141d71107c8
govardhan <govardhanxo@gmail.com>
2025-06-12 00:44:41 +0530

    roboshop

--- common.sh
+++ common.sh
@@ -61,6 +61,17 @@ maven_setup(){
     VALIDATE $? "Moving and renaming Jar file"
 }
 
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+    VALIDATE $? "Install Python3 packages"
+
+    pip3 install -r requirements.txt &>>$LOG_FILE
+    VALIDATE $? "Installing dependencies"
+
+    cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
+    VALIDATE $? "Copying Payement service"
+}
+
 systemd_setup(){
     cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
     VALIDATE $? "Copying $app_name service"
